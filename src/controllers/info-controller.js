49003584d4313ea3dd226f85ecb9be24da01819a
karanharshit <harsheet2020@gmail.com>
2023-12-07 22:18:29 +0530

    Added logger and basic setup of project

--- src/controllers/info-controller.js
+++ src/controllers/info-controller.js
@@ -1,5 +1,7 @@
+const { StatusCodes } = require('http-status-code');
+
 const info = (req, res) => {
-    return res.json({
+    return res.status(StatusCodes.OK).json({
         success: true,
         message: 'API is live',
         error: {},
