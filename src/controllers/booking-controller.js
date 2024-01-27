0b3bb09cbc47f86a19cf1c5e9ecdc883f38b9596
karanharshit <harsheet2020@gmail.com>
2024-01-27 19:17:16 +0530

    initial setup of api to book flight

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,1 +1,21 @@
-module.exports = {};
+const { BookingService } = require('../services');
+const { SuccessResponse, ErrorResponse } = require('../utils/common');
+
+async function createBooking(req, res) {
+    try{
+        const response = await BookingService.createBooking({
+            flightId: req.body.flightId,
+            userId: req.body.userId,
+            noOfSeats: req.body.noOfSeats,
+        })
+        SuccessResponse.data = response;
+        return res.status(200).json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res.status(error.statusCode).json(ErrorResponse);
+    }
+}
+
+module.exports = {
+    createBooking
+};
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,1 +1,23 @@
-module.exports = {};
+const axios = require('axios');
+
+const AppError = require('../utils/errors/app-error');
+const { BookingRepository } = require('../repositories');
+const { ServerConfig } = require('../config');
+const db = require('../models');
+
+async function createBooking(data) {
+    return new Promise((resolve, reject) => {
+        const result = db.sequelize.transaction(async function bookingImpl(t){
+            const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+            const flightData  = flight.data.data;
+            if(data.noOfSeats > flightData.totalSeats) {
+                reject(new AppError('Not enough seats available', 400));
+            }
+            resolve(true);
+        });
+    })
+} 
+
+module.exports = {
+    createBooking,
+};
